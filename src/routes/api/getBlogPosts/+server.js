631560296ad44c3bbc9e0b88c4401bfb5113316a
OrbitStorm <orbitstorm88@gmail.com>
2024-08-25 05:07:31 -0400

    Update +server.js

--- src/routes/api/getBlogPosts/+server.js
+++ src/routes/api/getBlogPosts/+server.js
@@ -7,9 +7,9 @@ export async function GET() {
     }
     const client = contentful.createClient({
         // This is the space ID. A space is like a project folder in Contentful terms
-        space: import.meta.env.VITE_CONTENTFUL_SPACE,
+        space: import.meta.env.CONTENTFUL_SPACE_ID,
         // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-        accessToken: import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN
+        accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN
     });
 	const data = await client.getEntries({content_type: 'blog_post'})
         .catch(e=> {
