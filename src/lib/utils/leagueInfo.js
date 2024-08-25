cd8301ed652f074c22db8460e7571668d3ee1c70
OrbitStorm <orbitstorm88@gmail.com>
2024-08-25 05:43:53 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -99,7 +99,7 @@ export const managers = [
     //  "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Pennsylvania", // (optional)
     //  "bio": "Lorem ipsum...",
-      "photo": "/managers/placeholder.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "https://sleepercdn.com/uploads/d35a7e91fcfd099932f421423c35b7d3.jpg", // square ratio recommended (no larger than 500x500)
     //  "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     //  "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
